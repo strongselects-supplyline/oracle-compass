c08b1bb69e9d57497d2f3f62007377d9f0d6689f
Ethan Payton <ethanpayton@Mac.lan>
2026-03-26 14:46:41 -0500

    fix(crm): added Next.js rewrites for CRM sub-app and excluded from global PWA cache

--- next.config.js
+++ next.config.js
@@ -4,10 +4,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  publicExcludes: ['!crm/**/*']
 })
 
 module.exports = withPWA({
   reactStrictMode: true,
   serverExternalPackages: ['googleapis'],
+  async rewrites() {
+    return [
+      { source: '/crm', destination: '/crm/index.html' },
+      { source: '/crm/', destination: '/crm/index.html' }
+    ]
+  }
 })
